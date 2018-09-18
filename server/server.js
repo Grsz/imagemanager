b7f83b06fa8db90517d4221a4c9fb994a8f606ee
Grsz <hrgergsz@gmail.com>
2018-09-18 21:23:00 +0100

    added files

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const { config } = require("./db");
+const db = require('knex')(config);
+const app = express();
+const path = require('path');
+const mime = require('mime');
+const fs = require('fs');
+
+const homePage = "http://localhost:3000/";
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(fileUpload());
+
+app.get('/', (req,res) => {
+    db('images').select('*')
+        .then(data => res.json(data))
+        .catch(err => console.log(err))
+})
+
+app.post('/upload', function(req, res) {
+    if (!req.files)
+      return res.status(400).send('No files were uploaded.');
+    let image = req.files.image;
+    image.mv('../public/images/' + image.name, err => {
+        if(err){return res.status(500).send(err);}
+        db('images').insert({
+            filename: image.name,
+            name: req.body.name,
+            type: "active"
+        }).then(() => res.redirect(homePage));
+    });
+  });
+app.post('/switchtype', (req, res) => {
+    db('images')
+    .where('id', req.body.id)
+    .update({type: req.body.type === "delete" ? "deleted" : "active"})
+    .then(() => res.status(200).send("Update successful"))
+    .catch(err => console.log(err))
+})
+app.get('/download/:filename', function(req, res){
+
+    var file = '../public/images/' + req.params.filename;
+    
+    var filename = path.basename(file);
+    var mimetype = mime.lookup(file);
+    
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-type', mimetype);
+    
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+    
+});
+app.listen(3001, () => {
+    console.log('running 3001')
+})
---
+++ src/App.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faDownload, faUndoAlt, faUpload } from '@fortawesome/free-solid-svg-icons';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      images: [],
+      target: null,
+      display: "active",
+      upload: false,
+      delete: false,
+    }
+  }
+  componentDidMount(){
+    fetch('http://localhost:3001/')
+    .then(res => res.json())
+    .then(images => this.setState({images}))
+    .catch(err => console.log(err))
+  }
+  footer = () => {
+    const { target, display, images } = this.state;
+    if(target){
+      if(display === "active"){
+        return <div className="bar">
+          <div className="delete" onClick={this.switchDelete}>
+            <FontAwesomeIcon icon={faTrashAlt} color="white" />
+          </div>
+          <a className="download" href={"http://localhost:3001/download/" + images.find(img => img.id === target).filename}>
+            <FontAwesomeIcon icon={faDownload} color="white" />
+          </a>
+        </div>
+      } else{
+        return <div className="bar">
+          <div className="restore" onClick={() => this.switchImgType("restore")}>
+            <FontAwesomeIcon icon={faUndoAlt} color="white" />
+          </div>
+        </div>
+      }
+    }
+  }
+  switchDelete = () => {
+    this.setState({delete: !this.state.delete})
+  }
+  setTarget = target => {
+    this.setState({target})
+  }
+  setDisplay = display => {
+    this.setState({display})
+  }
+  switchUpload = () => {
+    const { upload } = this.state;
+    this.setState({upload: !upload})
+  }
+  content = () => {
+    const { images, display } = this.state;
+    const displays = ["active", "deleted"];
+
+    const getImages = () => images
+      .filter(image => image.type === display)
+      .map(image =>
+        <div
+          key={image.id}
+          onClick={() => this.setTarget(image.id)}
+          className={this.state.target === image.id ? "target" : "nontarget"}
+        >
+          <img src={"images/" + image.filename} alt={image.id} />
+          <p className="name">{image.name}</p>
+        </div>
+    );
+    const getDisplays = () => displays.map(disp =>
+      <div 
+        className={display === disp ? "target" : "nontarget"} 
+        onClick={() => this.setDisplay(disp)}
+        key={disp}
+      >
+        <p className="displayName">{disp}</p>
+      </div>
+    );
+
+    return <div className="content">
+      <div className="displays">
+        {getDisplays()}
+      </div>
+      <div className="images">
+        {getImages()}
+      </div>
+    </div>
+  }
+  switchImgType = to => {
+    fetch('http://localhost:3001/switchtype', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          id: this.state.target,
+          type: to
+      })
+    }).then(() => {
+      const images = this.state.images.map(image => {
+        if(image.id === this.state.target){
+          return{...image,
+            type: to === "delete" ? "deleted" : "active"
+          }
+        } else {
+          return image
+        }
+      });
+      this.setState({images})
+      to === "delete" && this.switchDelete()
+    }).catch(err => console.log(err))
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>My Library</h1>
+          <div className="upload" onClick={this.switchUpload}>
+            <FontAwesomeIcon icon={faUpload} color="white" />
+          </div>
+        </header>
+        {this.content()}
+        {this.state.upload &&
+          <div className="cover">
+            <form ref='uploadForm' 
+              id='uploadForm' 
+              action='http://localhost:3001/upload' 
+              method='post' 
+              encType="multipart/form-data">
+                <input type="file" name="image" />
+                <input type="text" name="name" />
+                <input type='submit' value='Upload!' />
+                <button type="reset" value="Cancel" onClick={this.switchUpload}>Cancel</button>
+            </form>
+          </div>
+        }
+        {this.state.delete &&
+           <div className="cover">
+            <div className="msg">
+              <p>Are you sure?</p>
+              <div className="buttons">
+                <button onClick={this.switchDelete}>No</button>
+                <button onClick={() => this.switchImgType("delete")}>Yes</button>
+              </div>
+            </div>
+           </div>
+        }
+        <footer>
+          {this.footer()}
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
